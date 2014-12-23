4fc28e224b7b8e4ce4ea2a33a51f0ca0dc9b6573
Krevo <francois@crevola.org>
2014-12-23 17:00:47 +0100

    version 0.5

--- www/js/cbihelper.js
+++ www/js/cbihelper.js
@@ -1,5 +1,5 @@
-var CBI_VERSION = "Casio Basic Interpreter 0.4";
-var CBI_BUILD_DATE = "2014-12-18";
+var CBI_VERSION = "Casio Basic Interpreter 0.5";
+var CBI_BUILD_DATE = "2014-12-23";
 
 var TEXT_SCREEN_WIDTH = 21;
 var TEXT_SCREEN_HEIGHT = 7;

6411c092b687a90f810130a508115b257701b33c
François Crevola <francois@crevola.org>
2018-11-14 22:47:12 +0100

    Change version to a revision number (and keep build date) Add "Web" to make "Casio Basic Web Interpreter"

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-**"Casio Basic Interpreter"**
+**"Casio Basic Web Interpreter"**
 is a software able to interpret program written with the BASIC language used on casio calculators.
 Output is done on an HTML5 canvas.
 
--- generateBuildDate.sh
+++ generateBuildDate.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-TAG_DATE=`git log --tags --simplify-by-decoration --pretty="format:%ai %d" | head -n1 | cut -d' ' -f 1`
-sed 's/\(var CBI_BUILD_DATE\)\(.*\)/\1 = "'${TAG_DATE}'";/g' src/js/cbiversion.js_ > public/js/cbiversion.js
+BUILD_DATE=`date "+%Y-%m-%d"`
+sed 's/\(var CBI_BUILD_DATE\)\(.*\)/\1 = "'${BUILD_DATE}'";/g' src/js/cbiversion.js_ > public/js/cbiversion.js
 
-VERSION=`git describe --tags --abbrev=0` || exit 1
-sed -i 's/\(var CBI_VERSION\)\(.*\)/\1 = "'${VERSION}'";/g' public/js/cbiversion.js
+now=`date "+%s"`
+firstAug2014=`date -d '1 Aug 2014' "+%s"`
+REVISION="r$(($now - $firstAug2014))"
+sed -i 's/\(var CBI_VERSION\)\(.*\)/\1 = "'${REVISION}'";/g' public/js/cbiversion.js
